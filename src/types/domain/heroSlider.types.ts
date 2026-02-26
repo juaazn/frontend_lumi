7215eacdfb7e6f47eca5de2a7b1a14741b9ffe69
chuans <juaazn.05@gmail.com>
2026-02-26 23:26:29 +0100

    Cambio de directorio

---
+++ src/types/domain/heroSlider.types.ts
@@ -0,0 +1,11 @@
+export type HeroSlide = {
+  id: number;
+  title: string;
+  subtitle: string;
+  description: string;
+  order: number;
+  imageUrl: string;
+  alt: string;
+};
+
+export type HeroSlides = HeroSlide[];
