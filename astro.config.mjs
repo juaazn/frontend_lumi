5f2d5fee07a516f79b125ff8a21b311162a6ae93
Juan Diego Acosta Liscano <juaazn.05@gmail.com>
2026-03-22 18:40:32 +0100

    fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,55 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig, fontProviders } from "astro/config";
+import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Libre Caslon Condensed",
+      cssVariable: "--font-libre-caslon-condensed",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/LibreCaslonCondensed.woff2"],
+            weight: "200 900",
+            style: "normal",
+            display: "swap",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Instrument Sans",
+      cssVariable: "--font-instrument-sans",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/InstrumentSans-VariableFont_wdth,wght.woff2"],
+            weight: "200 900",
+            style: "normal",
+            display: "swap",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "SourGummy",
+      cssVariable: "--font-sour-gummy",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/SourGummy-Italic-VariableFont_wdth,wght.ttf"],
+            weight: "100 900",
+            style: "italic",
+            display: "swap",
+            stretch: "75% 125%",
+          },
+        ],
+      },
+    },
+  ],
 });
